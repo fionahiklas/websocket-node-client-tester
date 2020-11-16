9d4d734b84acde4424eefee878fb144b7de75989
Fiona Bianchi <fiona@hiklas.co.uk>
2020-11-16 12:30:29 +0000

    Fixing silly typo

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const Stomp = require('@stomp/stompjs');
 var cliArgs = process.argv.slice(2);
 console.log('Command line args: ', cliArgs);
 
-const wsUrl = argv[0];
-const wsSubscribeTopic = argv[1];
+const wsUrl = cliArgs[0];
+const wsSubscribeTopic = cliArgs[1];
 
 console.log('Creating STOMP Client with WS URL: ', wsUrl);
 const stompClient = Stomp.client(wsUrl);
